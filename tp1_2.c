5eee3099a47860c57cad048c8e09f5f7fcfaa61f
NazaretCS <nazaretcampos27@gmail.com>
2023-03-22 15:49:55 -0300

    ejercicio 2.4 completado

--- tp1_2.c
+++ tp1_2.c
@@ -6,6 +6,8 @@ int Cuadrado (int num);
 void CuadradoTipoVoid (int num);
 void MostrarDireccionYContenido( int *punt);
 void Invertir( int *a, int *b);
+void orden(int *a, int *b);
+
 
 int main(){
 
@@ -15,30 +17,39 @@ int main(){
 
 
     /* Funcion con Retorno */
-    num2 = Cuadrado(num1);    
-    printf("El resultado de elevar ese numero al cuadrado es: %d \n\n ", num2);
-
+        num2 = Cuadrado(num1);    
+        printf("El resultado de elevar ese numero al cuadrado es: %d \n\n", num2);
+    /* ----------------------- */
 
     /* Funcion sin Retorno */
-    CuadradoTipoVoid(num1);
+        CuadradoTipoVoid(num1);
+    /* ----------------------- */
+
 
 
     /* Funcion para mostrar el contenido y la direccion de la variable */
-    MostrarDireccionYContenido( &num1 );
-
+        MostrarDireccionYContenido( &num1 );
+    /* ----------------------- */
     
+
     /* Funcion Para intercambiar el valor de las varaibles */
-    printf("Por favor ingrese un valor para la variable a: \n");
-    scanf("%d", &a);
-    
-    printf("Por favor ingrese un valor para la variable b: \n");
-    scanf("%d", &b);
+        printf("Por favor ingrese un valor para la variable a: \n");
+        scanf("%d", &a);
+        
+        printf("Por favor ingrese un valor para la variable b: \n");
+        scanf("%d", &b);
 
-    Invertir(&a, &b);
+        Invertir(&a, &b);
 
-    printf("El valor de la variable a es: %d \n\n", a);
-    printf("El valor de la variable b es: %d \n\n", b);
+        printf("El valor de la variable a es: %d \n\n", a);
+        printf("El valor de la variable b es: %d \n\n", b);
+    /* ----------------------- */
 
+    /* Funcion que devuelve el valor mas alto en la variable a, y el valor mas bajo en la variable b */
+        orden(&a, &b);
+        printf("La variable con el valor mas pequenio es %d \n\n", a);
+        printf("La variable con el valor mas GRANDE es %d \n\n", b);
+    /* ----------------------- */
     return 0;
 }
 
@@ -74,3 +85,14 @@ void Invertir( int *a, int *b) //deberá devolver en el valor de a en la variabl
     *b = auxiliar;   
 }
 
+/* e) Dado dos parámetros de entrada, deberá devolverlos de forma ordenada, en el primer parámetro el menor y en el segundo el mayor. */
+void orden(int *a, int *b) //deberá devolver en el valor de a el valor más chico, y en el valor de b el valor más grande
+{
+    int auxiliar;
+    if (*a > *b)
+    {
+        auxiliar = *b;
+        *b = *a;
+        *a = auxiliar;
+    }
+}
